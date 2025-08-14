e5535fb9ab709b52eb819de669f6a8cc2c12d1b0
ilaydaberrak <ilayda.berrak.cakal@fimple.co.uk>
2025-08-14 12:27:03 +0300

    ilk sürüm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kaç tane sayı çifti giriceksiniz?");
+        int n = int.Parse(Console.ReadLine() ?? "0");
+
+        int[,] sayilar = new int[n, 2]; //n satır, her satırda 2 sayı
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"{i + 1}. çiftin 1.sayısını girin:");
+            sayilar[i, 0] = int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine($"{i + 1}. çiftin 2.sayısını girin:");
+            sayilar[i, 1] = int.Parse(Console.ReadLine() ?? "0");
+        }
+        Console.WriteLine("\nSonuçlar:");
+        for (int i = 0; i < n; i++)
+        {
+            int toplam = sayilar[i, 0] + sayilar[i, 1];
+            if (sayilar[i, 0] == sayilar[i, 1])
+            {
+                Console.WriteLine(Math.Pow(toplam, 2));
+            }
+            else
+            {
+                Console.WriteLine(toplam);
+            }
+        }
+    }
+}
